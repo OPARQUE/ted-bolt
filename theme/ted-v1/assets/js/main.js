fb6761c7e7083d43da905318e89ee2d4d3735a03
caiooparque <caiosouza@oparque.art.br>
2015-07-06 11:19:24 -0300

    Corrigido slider
    Corrigido slider

--- theme/ted-v1/assets/js/main.js
+++ theme/ted-v1/assets/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         slidesToScroll: 1,
         pauseOnHover: true,
         arrows: true,
-        prevArrow: "<a class='nav-arrow-left'><img src='assets/img/arrow-left.png' /></a>",
-        nextArrow: "<a class='nav-arrow-right'><img src='assets/img/arrow-right.png' /></a>"
+        prevArrow: "<a class='nav-arrow-left'><img src='/theme/ted-v1/assets/img/arrow-left.png' /></a>",
+        nextArrow: "<a class='nav-arrow-right'><img src='/theme/ted-v1/assets/img/arrow-right.png' /></a>"
     });
 });
